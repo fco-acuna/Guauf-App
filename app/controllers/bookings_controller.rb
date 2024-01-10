d231740906e527ef754e98b784ab4bd0282f225d
Francisco Acuña <facuna490@gmail.com>
2024-01-09 21:00:47 -0600

    controlador de bookings

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,37 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+    @service = Service.find(params[:service_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @service = Service.find(params[:service_id])
+    @booking.service = @service
+    if @booking.save!
+      redirect_to service_path(@service)
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:time, :price, :date, :dog_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+    devise_for :users
+    root to: "pages#home"
+  
+  
+    resources :walkers do
+      resources :services, only: [:new, :create]
+    end
+    resources :services, except: [:new, :create] do
+      resources :bookings, only: [:new, :create]
+    end
 end
